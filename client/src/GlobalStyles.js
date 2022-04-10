250e6e833e7ee274c3dd1477f0872ba4034c6965
bonchevskyi <roman.bonchevskyi@gmail.com>
2022-04-10 21:24:51 +0200

    globalstyles CMT

--- client/src/GlobalStyles.js
+++ client/src/GlobalStyles.js
@@ -10,20 +10,16 @@ const GlobalStyles = createGlobalStyle`
 *{
   box-sizing: border-box;
   margin: 0;
-  //padding: 0;
   scroll-behavior: smooth;
   font-family: 'Trebuchet MS', sans-serif;  
-  //font-size: 1em;
   //font-size: 1em;
 
   &::-webkit-scrollbar {
   width: 0.5vw;
-  // =======
 }
 
 &::-webkit-scrollbar-track {
   background-color: ${DEFAULT};
-  //box-shadow: inset 0 0 10px ${DARK};
   background-clip: content-box;
 
 }
@@ -42,96 +38,37 @@ const GlobalStyles = createGlobalStyle`
 }
 
 body{
-  //min-height: 100vh;
   height: 100%;
-  
-  //display: flex;
-  //flex-direction: column;
-  //background: #1b5c3e;   
-  //background: ${DEFAULT};  
 }
 `;
 
 export const Container = styled.div`
   z-index: 1;
   display: flex;
-  flex-direction: column; //NEW
+  flex-direction: column;
   justify-content: ${(props) =>
     props.position === 'centered' ? 'center' : 'normal'};
   align-items: ${(props) => (props.align === 'centered' ? 'center' : 'normal')};
-  //min-height: 401px; //WAS
-
-  //min-height: calc(100vh - 80px - 300px);
-  /* min-height: 50vh; */
   width: 100%;
-  max-width: 70vw; //CHEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEECK
-  //margin: auto;
-  //margin-top: 30px;
-  //margin-bottom: 30px;
-  //margin-top: auto;
-
+  max-width: 70vw; //CHECK
   margin-right: auto;
   margin-left: auto;
-
-  //background: green; //NEW
-  //border: 5px solid green;
-
-  /* position: relative; */
-  /* padding-bottom: 60px; */
-  /* was padding 200px */
-
   min-height: calc(100vh - 80px - 300px - 10px - 10px);
 
   @media screen and (max-width: 890px) {
-    //WAS 768 px
-    //min-height: 100vh;
-    max-width: 100vw; //CHEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEECK
+    max-width: 100vw; //CHECK
     padding-left: 0px;
     padding-right: 0px;
     margin-right: auto;
     margin-left: auto;
-    //background: red;
   }
 `;
 
 export const WrapGlobal = styled.div`
-  //color: #fff;
-  //padding: 48px 0 48px 0;
   padding-top: 10px;
   padding-bottom: 10px;
   background: ${DEFAULT};
-  //min-height: calc(100vh - 80px - 300px);
 `;
-
-// export const CenterGlobal = styled.div`
-//   display: flex;
-//   flex-direction: column; //NEW
-//   justify-content: center;
-//   align-items: center;
-// `;
-
-// export const ButtonNav = styled.button`
-//   width: 125px;
-//   border-radius: 4px;
-//   background: ${({ primary }) => (primary ? '#fff' : '#1b5c3e')};
-//   white-space: nowrap;
-//   padding: ${({ paddBig }) => (paddBig ? '12px 64px' : '10px 20px')};
-//   color: black;
-//   font-size: ${({ fontBig }) => (fontBig ? '20px' : '16px')};
-//   outline: none;
-//   border: none;
-//   cursor: pointer;
-
-//   &:hover {
-//     transition: all 0.3s ease-out;
-//     background: #fff;
-//     background: ${({ primary }) => (primary ? '#2e9c69' : '#0D2D10')};
-//   }
-
-//   @media screen and (max-width: 960px) {
-//     width: 100%;
-//   }
-// `;
 
 export const Button = styled.button`
   width: fit-content;
