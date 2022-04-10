4f9a380763923a6b1b4c9ec7d06fdbaad142d250
bonchevskyi <roman.bonchevskyi@gmail.com>
2022-04-10 19:26:33 +0200

    dashboard CMT

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,22 +1,17 @@
 import React, { useEffect } from 'react';
-import {
-  Link,
-  useSearchParams,
-  useParams,
-  useLocation,
-} from 'react-router-dom';
+import { useSearchParams, useLocation } from 'react-router-dom';
 import { Container, WrapGlobal } from '../../GlobalStyles';
 import { useState } from 'react';
-//import SelectSearch from 'react-select-search';
-//import AsyncSelect from 'react-select/async';
-import Select from 'react-select';
 import axios from 'axios';
 import Loading from '../../components/Loading/Loading';
-import Drop from '../../components/Dropdown/Drop';
+import { InfoSection } from '../../components';
+import {
+  dashboardLandingObj,
+  dashboardNoDataObj,
+} from '../../components/InfoSection/Data';
 
 import {
   CustomSelect,
-  DataContainer,
   SelectsContainer,
   WrapSingleSelect,
   WrapCitySelect,
@@ -28,74 +23,35 @@ import {
   DataPostTitle,
   DataPostText,
   DataInfoDiv,
-  WrapRightBoth,
   PostStats,
   DataPostPath,
-  WrapRightBottom,
   WrapRightTop,
-  WrapBoth,
-  ButtonFull,
-  HoverData,
-  HoverDiv,
   DataTopWrap,
   DataPostButton,
 } from './Dashboard.styled';
-
-import { Avatar, AvatarDiv } from '../Profile/Profile.styled';
-import { InfoSection } from '../../components';
-import {
-  dashboardLandingObj,
-  dashboardNoDataObj,
-} from '../../components/InfoSection/Data';
 
 const Dashboard = () => {
   const [isDisabledUni, setisDisabledUni] = useState(true);
   const [isDisabledDep, setisDisabledDep] = useState(true);
   const [isDataZero, setisDataZero] = useState(false);
 
-  const [newCreated, setNewCreated] = useState('');
-  const [newUpdated, setNewUpdated] = useState('');
-
   const [defaultCityLabel, setDefaultCityLabel] = useState('');
   const [defaultUniversityLabel, setDefaultUniversityLabel] = useState('');
   const [defaultDepartmentLabel, setDefaultDepartmentLabel] = useState('');
 
-  const [showDiv, setShowDiv] = useState(false);
-
-  //var urlParams = useParams();
-  //console.log('=============== PARAMS IN DASH: ', params);
-
   const { search } = useLocation();
-  console.log('=========SEARCH LOCATION PARAMS IN DASH', search);
 
   const [searchParams, setSearchParams] = useSearchParams({});
-  const [currentParams, setCurrentParams] = useSearchParams({});
 
   const cityParam = parseInt(searchParams.get('city'));
   const uniParam = parseInt(searchParams.get('university'));
   const depParam = parseInt(searchParams.get('department'));
 
-  console.log('SEARCH PARAMS: ', [cityParam, uniParam, depParam]);
-
-  //console.log('ENTRIES');
-  //console.log(searchParams.entries());
-
-  //let params = useSearchParams({});
-
-  //const [searchParams] = useSearchParams();
-
-  const [inputVal, setInputVal] = useState('');
-
-  const [inputValue, setInputValue] = useState('');
-  const [value, setValue] = useState();
-
   const [isLoading, setIsLoadiing] = useState(false);
   const loadScreen = (timeMS) => {
     setIsLoadiing(true);
-    //console.log('LOADING STARTED');
     setTimeout(() => {
       setIsLoadiing(false);
-      //console.log('LOADING ENDED');
     }, timeMS);
   };
 
@@ -148,11 +104,6 @@ const Dashboard = () => {
       value: d.id,
       label: d.name,
     }));
-
-    // if (cityParam == options?.value) {
-    //   console.log('BULOOOOOOOOOOOOOOOOO');
-    //   setDefaultCityLabel();
-    // }
     setCityOptions(options);
   };
 
@@ -163,14 +114,8 @@ const Dashboard = () => {
   };
 
   const handleChangeCity = (e) => {
-    console.log('City was selected');
-    //params.push('city', e.value);
+    //console.log('City was selected');
     setSearchParams({ city: e.value });
-    //setInputVal(inputUni.name);
-    //setInputVal(selectedUni.value);
-    //setInputVal('');
-
-    //check
     setInputCity({ id: e.value, name: e.label });
     if (e.value) getUniversities(e.value);
     setInputUni({});
@@ -180,9 +125,6 @@ const Dashboard = () => {
     setInputDep({});
     setDepOptions(initialValuesDep);
     setisDisabledDep(true);
-
-    //document.getElementById('city_label_ID').innerHTML = 'Item to be uploaded';
-    //setUniPlaceholder('Zmiana');
   };
 
   // =========== FOR UNIVERSITY ===========
@@ -208,12 +150,7 @@ const Dashboard = () => {
   };
 
   const handleChangeUniversity = (e) => {
-    console.log('Uni was selected');
-    //params.push('university', e.value);
-    //setSearchParams({ university: e.value });
-    //setSearchParamsZ({ university: e.value });
-    //setSearchParamsZ({ university: e.value });
-    //setSearchParamsZ({ university: e.value });
+    //console.log('Uni was selected');
     setSearchParams({
       city: inputCity.id,
       university: e.value,
@@ -251,10 +188,7 @@ const Dashboard = () => {
   };
 
   const handleChangeDepartment = (e) => {
-    console.log('Dep was selected');
-
-    //params.push('department', e.value);
-    //setSearchParams({ department: e.value });
+    //console.log('Dep was selected');
     setSearchParams({
       city: inputCity.id,
       university: inputUni.id,
@@ -263,14 +197,6 @@ const Dashboard = () => {
     setInputDep({ id: e.value, name: e.label });
   };
 
-  // // load options using API call
-  // const loadOptionsCities = () => {
-  //   const response = axios.get('/get-cities');
-  //   const json = JSON.stringify(response.data);
-  //   console.log('LOADED DATA: ', json);
-  //   return json;
-  // };
-
   // =========== FOR SEARCH | POSTS ===========
 
   const [posts, setPosts] = useState({});
@@ -278,14 +204,10 @@ const Dashboard = () => {
 
   const handleSearch = async () => {
     loadScreen(700);
-    //setisDataZero(false);
 
     setPosts({});
-    //console.log([...params.entries()]);
-    console.log('SEARCH!');
     setisDisabledUni(true);
     setisDisabledDep(true);
-    //console.log(inputCity);
 
     if (
       Object.keys(inputCity).length > 0 ||
@@ -295,16 +217,11 @@ const Dashboard = () => {
       uniParam ||
       depParam
     ) {
-      //setSearchParams({});
-      console.log('WE WANT TO SEARCH!');
-      //var objToSend = { inputCity, inputUni, inputDep };
       setInputCity({});
       setInputUni({});
       setInputDep({});
       var sParams = Object.fromEntries([...searchParams]);
-      var stringParams = Object.entries(sParams);
-      console.log('SEARCH PARAMS BEFORE AXIOS:', stringParams);
-      console.log('CITY PARAMS BEFORE AXIOS:', sParams?.city);
+      //var stringParams = Object.entries(sParams);
       try {
         const response = await axios.post('/search', null, {
           params: {
@@ -314,26 +231,16 @@ const Dashboard = () => {
           },
         });
         const data = response.data;
-        console.log('DATA === ', data);
         if (data.length == 0) {
           setisDataZero(true);
-          console.log('<<<<< DATA IS 0');
         }
         const psts = Array.from(data);
-        console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! PSTS created', psts);
-
-        //setNewCreated(TMPnewCreated);
-        //setNewUpdated(TMPnewUpdated);
         setPosts(psts);
-        console.log('=====================DATA', data[0]);
       } catch (error) {
         console.log('Search data ERROR: ', error);
       }
       setCanRender(true);
-      //setInputDep({ id: e.value, name: e.label });
     }
-
-    //loadScreen(1000);
   };
 
   useEffect(() => {
@@ -343,45 +250,13 @@ const Dashboard = () => {
     getDefaultUniversityLabel();
     getDefaultDepartmentLabel();
     setCanRender(false);
-    //setInputVal('STARTED');
-    //setSearchParams({});
     setisDisabledUni(true);
     setisDisabledDep(true);
-    //setPosts({});
 
     return () => {
-      console.log(
-        '!!!!!!!!!!!!!!!!!!!!!!!!!!CLEANUP DASHBOARD!!!!!!!!!!!!!!!!!!!'
-      );
-      // setInputCity({});
-      // setCityOptions(initialValuesCity);
-      // setInputUni({});
-      // setUniOptions(initialValuesUni);
-      // setInputDep({});
-      // setDepOptions(initialValuesDep);
-      setNewCreated('');
-      setNewUpdated('');
       setIsLoadiing(false);
     };
   }, []);
-
-  // useEffect(() => {
-  //   setCurrentParams(Object.fromEntries([...searchParams]));
-  //   console.log('CURRENT PARAMS:', currentParams); // get new values onchange
-  // }, [searchParams]);
-
-  // useEffect(() => {
-  //   console.log('INPUT CITY CHANGED!');
-  //   setInputUni({});
-  //   setUniOptions(initialValuesUni);
-
-  //   return () => {
-  //     console.log(
-  //       '!!!!!!!!!!!!!!!!!!!!!!!!!!CLEANUP ON UNI!!!!!!!!!!!!!!!!!!!'
-  //     );
-  //     //setIsLoadiing(true);
-  //   };
-  // }, [inputCity]);
 
   var imageName = require('../../images/avatar-nobg-white.png');
 
@@ -404,24 +279,16 @@ const Dashboard = () => {
                 <WrapCitySelect>
                   <CustomSelect
                     classNamePrefix={'Select'}
-                    //name='city'
                     onMenuOpen={handleMenuOpenCity}
                     options={selectedCity}
                     onChange={handleChangeCity}
                     placeholder={'Wybierz Miasto'}
                     noOptionsMessage={() => 'Nie znaleziono opcji!'}
-                    //email = el)}
-                    // cacheOptions
-                    // defaultOptions
                     value={selectedCity.value}
                     defaultValue={{
                       label: defaultCityLabel || 'Wybierz Miasto',
                       value: cityParam,
                     }}
-                    //getOptionLabel={(e) => console.log(e)}
-                    //getOptionValue={(e) => console.log(e)}
-                    // loadOptions={cityList}
-                    // onInputChange={handleInputChange}
                   />
                 </WrapCitySelect>
                 <br />
@@ -439,11 +306,6 @@ const Dashboard = () => {
                       label: defaultUniversityLabel || 'Wybierz Uniwersytet',
                       value: uniParam,
                     }}
-                    //getOptionLabel={(e) => console.log(e)}
-                    //getOptionValue={(e) => console.log(e)}
-                    // loadOptions={cityList}
-                    // onInputChange={handleInputChange}
-                    // onChange={handleChange}
                   />
                 </WrapSingleSelect>
                 <br />
@@ -455,33 +317,17 @@ const Dashboard = () => {
                     placeholder={'Wybierz Wydział'}
                     noOptionsMessage={() => 'Nie znaleziono opcji!'}
                     isDisabled={isDisabledDep}
-                    // cacheOptions
-                    // defaultOptions
                     value={selectedDep.value}
                     defaultValue={{
                       label: defaultDepartmentLabel || 'Wybierz Wydział',
                       value: depParam,
                     }}
-                    //getOptionLabel={(e) => console.log(e)}
-                    //getOptionValue={(e) => console.log(e)}
-                    // loadOptions={cityList}
-                    // onInputChange={handleInputChange}
-                    // onChange={handleChange}
                   />
                 </WrapSingleSelect>
                 <br />
                 <SearchButton onClick={handleSearch}>SZUKAJ</SearchButton>
               </SelectsContainer>
             </Search>
-
-            {/* <p>CHOSEN CITY VALUE NAME: {inputCity.name}</p>
-            <p>CHOSEN CITY VALUE ID: {inputCity.id}</p>
-
-            <p>CHOSEN UNI VALUE NAME: {inputUni.name}</p>
-            <p>CHOSEN UNI VALUE ID: {inputUni.id}</p>
-
-            <p>CHOSEN DEP VALUE NAME: {inputDep.name}</p>
-            <p>CHOSEN DEP VALUE ID: {inputDep.id}</p> */}
 
             <br />
 
