fed1da1fad24964cf82eb342b7bb806f8ed96eff
bonchevskyi <roman.bonchevskyi@gmail.com>
2022-04-10 20:45:06 +0200

    scroll CMT

--- client/src/components/ScrollToTop.js
+++ client/src/components/ScrollToTop.js
@@ -4,8 +4,6 @@ import { useLocation } from 'react-router-dom';
 export default function ScrollToTop() {
   const { pathname } = useLocation();
 
-  //console.log('PATHNAME in SCROLLTOTOP', pathname);
-
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
